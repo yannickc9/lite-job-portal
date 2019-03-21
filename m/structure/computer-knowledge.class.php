2395c169daf5f25c1cc6a9d1b4a581975342a978
Yannick Choni <yannickjulberts@gmail.com>
2019-03-21 21:14:18 +0200

    (new file m/structure/computer-knowledge) create ComputerKnowledge class : id, id_cv, type, knowledge

--- m/structure/computer-knowledge.class.php
+++ m/structure/computer-knowledge.class.php
@@ -14,4 +14,31 @@
  */
 class ComputerKnowledge {
 //put your code here
+    private $id;
+    private $id_cv;
+    private $type;
+    private $knowledge;
+    
+    public function __construct($id, $id_cv, $type, $knowledge) {
+        $this->id = $id;
+        $this->id_cv = $id_cv;
+        $this->type = $type;
+        $this->knowledge = $knowledge;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getIdCv() {
+        return $this->id_cv;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function getKnowledge() {
+        return $this->knowledge;
+    }
 }
