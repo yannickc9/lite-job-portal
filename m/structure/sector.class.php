d0877b0cc54dcc2337487ef9b6915169f06d3828
Yannick Choni <yannickjulberts@gmail.com>
2019-03-22 04:21:09 +0200

    (new file m/structure/sector.class.php) create Sector class : id,name,description

---
+++ m/structure/sector.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of sector
+ *
+ * @author Choni
+ */
+class sector {
+    //put your code here
+    private $id;
+    private $name;
+    private $description;
+    
+    public function __construct($id, $name, $description) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+}
