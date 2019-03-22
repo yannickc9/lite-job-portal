e34c36184319337c8cf5b8774ce10b6c3b838977
Yannick Choni <yannickjulberts@gmail.com>
2019-03-22 03:10:46 +0200

    (new file m/structure/hobby.class.php) create Hobby class : id,id_cv,description

---
+++ m/structure/hobby.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of hobby
+ *
+ * @author Choni
+ */
+class Hobby {
+    //put your code here
+    private $id;
+    private $id_cv;
+    private $description;
+    
+    public function __construct($id, $id_cv, $description) {
+        $this->id = $id;
+        $this->id_cv = $id_cv;
+        $this->description = $description;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getId_cv() {
+        return $this->id_cv;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+}
