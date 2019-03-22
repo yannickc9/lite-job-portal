9f0fcf653d823d4eba12c3ce882dc526c2c6d505
Yannick Choni <yannickjulberts@gmail.com>
2019-03-22 03:44:11 +0200

    (update file m/structure/language.class.php) create Language class : is,id_cv,iso_code

---
+++ m/structure/language.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of language
+ *
+ * @author Choni
+ */
+class Language {
+    //put your code here
+    private $id;
+    private $id_cv;
+    private $iso_code;
+    
+    public function __construct($id, $id_cv, $iso_code) {
+        $this->id = $id;
+        $this->id_cv = $id_cv;
+        $this->iso_code = $iso_code;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getId_cv() {
+        return $this->id_cv;
+    }
+
+    public function getIsoCode() {
+        return $this->iso_code;
+    }
+}
