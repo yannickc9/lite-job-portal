5551b4ea70ccf43b1a3d7a8a2e1629897fee0dad
Yannick Choni <yannickjulberts@gmail.com>
2019-03-23 22:07:55 +0200

    (new file m/model/skill.manager.dao.php) create SkillManager model class : __construct(),add(CV,Skill),update(CV,Skill),remove(CV,Skill)

---
+++ m/model/skill.manager.dao.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of cv
+ *
+ * @author Choni
+ */
+class SkillManager extends Manager{
+    //put your code here
+    public function __construct(){
+        parent::__construct();
+    }
+
+
+    public function add(CV $cv, Skill $skill){
+        # code...
+    }
+
+    public function update(CV $cv, Skill $skill){
+       # code...
+    }
+
+    public function remove(CV $cv, Skill $skill){
+        # code...
+    }
+}
