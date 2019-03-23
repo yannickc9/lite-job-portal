721c738915eaabb438ca3445eafc0194fb1f7bcc
Yannick Choni <yannickjulberts@gmail.com>
2019-03-23 22:03:11 +0200

    (new file m/model/experience.manager.dao.php) create ExperienceManager model class : __construct(),add(CV,Formation),update(CV,Formation),remove(CV,Formation)

---
+++ m/model/experience.manager.dao.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of cv
+ *
+ * @author Choni
+ */
+class ExperienceManager extends Manager{
+    //put your code here
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function add(CV $cv, Experience $experience){
+        # code...
+    }
+
+    public function update(CV $cv, Experience $experience){
+       # code...
+    }
+
+    public function remove(CV $cv, Experience $experience){
+        # code...
+    }
+}
