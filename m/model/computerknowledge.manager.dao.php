49248fdd2efd90f4f7411cb3487fd7b41b3e83ed
Yannick Choni <yannickjulberts@gmail.com>
2019-03-23 22:14:45 +0200

    (new file m/model/computerknowledge.manager.dao.php) create ComputerKnowledgeManager model class : __construct(),add(CV,ComputerKnowledge),remove(CV,ComputerKnowledge)

---
+++ m/model/computerknowledge.manager.dao.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of cv
+ *
+ * @author Choni
+ */
+class ComputerKnowledgeManager extends Manager{
+    //put your code here
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function add(CV $cv, ComputerKnowledge $computer_knowledge){
+        # code...
+    }
+
+    public function remove(CV $cv, ComputerKnowledge $computer_knowledge){
+        # code...
+    }
+}
