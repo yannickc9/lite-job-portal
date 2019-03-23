7b1c1a5252e4911b6d100c20281711b595e11a9a
Yannick Choni <yannickjulberts@gmail.com>
2019-03-23 21:56:33 +0200

    (new file m/model/formation.manager.dao.php) create FormationManager model class : __construct(),add(CV,Formation),remove(CV,Formation)

---
+++ m/model/formation.manager.dao.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of cv
+ *
+ * @author Choni
+ */
+class FormationManager extends Manager{
+    //put your code here
+    public function __construct(){
+        parent::__construct();
+    }
+
+    
+    public function add(CV $cv, Formation $formation){
+        # code...
+    }
+
+    public function remove(CV $cv, Formation $formation){
+        # code...
+    }
+}
