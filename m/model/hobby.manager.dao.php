9e2b40002f4f73bcfe1f3853a3e2f258acb63590
Yannick Choni <yannickjulberts@gmail.com>
2019-03-23 22:21:54 +0200

    (new file m/model/hobby.manager.dao.php) create HobbyManager model class : __construct(),add(CV,Hobby),remove(CV,Hobby)

---
+++ m/model/hobby.manager.dao.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of cv
+ *
+ * @author Choni
+ */
+class HobbyManager extends Manager{
+    //put your code here
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function add(CV $cv, Hobby $hobby){
+        # code...
+    }
+
+    public function update(CV $cv, Hobby $hobby){
+     
+       # code...
+    }
+
+    public function remove(CV $cv, Hobby $hobby){
+        # code...
+    }
+}
