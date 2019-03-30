a161ad9b618e13796bc4648613ceb65e3ff0d5b8
Yannick Choni <yannickjulberts@gmail.com>
2019-03-30 16:19:59 +0200

    (update file m/model/formation.manager.dao.php) modify add(cv,formation) method,add transaction instructions

--- m/model/formation.manager.dao.php
+++ m/model/formation.manager.dao.php
@@ -19,8 +19,9 @@ class FormationManager extends Manager{
 
     
     public function add(CV $cv, Formation $formation){
+        $this->pdo->beginTransaction();
         $request = $this->pdo->prepare( 'INSERT INTO formation(id_cv,name,institute,document,location,start_date,end_date) VALUES(:id_cv,:name,:institute,:document,:location,:start_date,:end_date)');
-        return $request->execute(array(
+        $request->execute(array(
             'id_cv' => $cv->getIdAccount(),
             'name' => $formation->getName(),
             'institute' => $formation->getInstitute(),
@@ -29,6 +30,13 @@ class FormationManager extends Manager{
             'start_date' => $formation->getStartDate(),
             'end_date' => $formation->getEndDate()
         ));
+        $request = $this->pdo->prepare('UPDATE cv SET last_update_datetime WHERE id_account = :id_account');
+        $request->execute(array(
+            'id_account' => $cv->getIdAccount()
+        ));
+        if(!$this->pdo->commit())
+            return !$this->pdo->rollBack();
+        return true;
     }
 
     public function remove(CV $cv, Formation $formation){
