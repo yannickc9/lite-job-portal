c06e4ed62e2f4fc86b3c0741dc682b01694dd2a6
Yannick Choni <yannickjulberts@gmail.com>
2019-03-30 17:17:40 +0200

    (update file m/model/skill.manager.dao.php) implement add(cv,skill)

--- m/model/skill.manager.dao.php
+++ m/model/skill.manager.dao.php
@@ -19,7 +19,19 @@ class SkillManager extends Manager{
 
 
     public function add(CV $cv, Skill $skill){
-        # code...
+        $this->pdo->beginTransaction();
+        $request = $this->pdo->prepare('INSERT INTO level(id_cv,description) VALUES(:id_cv,:description)');
+        $request->execute(array(
+            'id_cv' => $cv->getIdAccount(),
+            'description' => $skill->getDescription()
+        ));
+        $request = $this->pdo->prepare('UPDATE cv SET last_update_datetime = NOW() WHERE id_account = :id_account');
+        $request->execute(array(
+            'id_account' => $cv->getIdAccount()
+        ));
+        if(!$this->pdo->commit())
+            return !$this->pdo->rollBack();
+        return true;
     }
 
     public function update(CV $cv, Skill $skill){
