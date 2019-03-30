421c23a411a31b0a044dbcd582ba9c29bef37abf
Yannick Choni <yannickjulberts@gmail.com>
2019-03-30 15:27:20 +0200

    (new file m/model/level.manager.dao.php) add LevelManager

---
+++ m/model/level.manager.dao.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of cv
+ *
+ * @author Choni
+ */
+class LevelManager extends Manager{
+    //put your code here
+    public function __construct(){
+        parent::__construct();
+    }
+
+
+    public function add(CV $cv, Level $level){
+        # code...
+    }
+
+    public function update(CV $cv, Level $level){
+        # code...
+    }
+
+    public function remove(CV $cv, Level $level){
+        # code...
+    }
+}
