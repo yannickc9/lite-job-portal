234649bc926d01c5b3bdfe6f6497a179cea666fa
Yannick Choni <yannickjulberts@gmail.com>
2019-03-30 18:03:45 +0200

    (update file m/model/computerknowledge.manager.dao.php) implement add(cv,computer_knowledge)

--- m/model/computerknowledge.manager.dao.php
+++ m/model/computerknowledge.manager.dao.php
@@ -18,7 +18,20 @@ class ComputerKnowledgeManager extends Manager{
     }
 
     public function add(CV $cv, ComputerKnowledge $computer_knowledge){
-        
+        $this->pdo->beginTransaction();
+        $request = $this->pdo->prepare('INSERT INTO computer_knowledge(id_cv,type,knowledge) VALUES(:id_cv,:type,:knowledge)');
+        $request->execute(array(
+            'id_cv' => $cv->getId(),
+            'type' => $computer_knowledge->getType(),
+            'knowledge' => $computer_knowledge->getKnowledge()
+        ));
+        $request = $this->pdo->prepare('UPDATE cv SET last_update_datetime = NOW() WHERE id_account = :id_account');
+        $request->execute(array(
+            'id_account' => $cv->getIdAccount()
+        ));
+        if(!$this->pdo->commit())
+            return !$this->pdo->rollBack();
+        return true;
     }
 
     public function remove(CV $cv, ComputerKnowledge $computer_knowledge){
