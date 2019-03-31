0e2ca35ebb02f029d817cebcf163e5394a224d27
Yannick Choni <yannickjulberts@gmail.com>
2019-03-31 03:13:51 +0200

    (update file m/model/employer.manager.dao.php) implement add(employer,society) method

--- m/model/employer.manager.dao.php
+++ m/model/employer.manager.dao.php
@@ -19,7 +19,20 @@ class EmployerManager extends Manager{
 
 
     public function add(Employer $employer,Society $society){
-        
+        $request = $this->pdo->prepare( 'INSERT INTO society(id_employer,id_sector,name,description,address,location,telephone,email,website,employees,type) VALUES(:id_employer,:id_sector,:name,:description,:address,:location,:telephone,:email,:website,:employees,:type)');
+        return $request->execute(array(
+            'id_employer' => $employer->getId(),
+            'id_sector' => $society->getIdSector(),
+            'name' => $society->getName(),
+            'description' => $society->getDescription(),
+            'address' => $society->getAddess(),
+            'location' => $society->getLocation(),
+            'telephone' => $society->getTelephone(),
+            'email' => $society->getEmail(),
+            'website' => $society->getWebsite(),
+            'employees' => $society->getEmployees(),
+            'type' => $society->getType() 
+        ));
     }
 
     public function update(Society $society){
