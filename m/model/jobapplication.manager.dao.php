5228573f0c8491a749c99d28153f7b01320e66c3
Yannick Choni <yannickjulberts@gmail.com>
2019-04-12 17:00:50 +0200

    (update m/model/jobapplication.manager.dao.php) implement getJobApplications method

--- m/model/jobapplication.manager.dao.php
+++ m/model/jobapplication.manager.dao.php
@@ -34,8 +34,43 @@ class JobApplicationManager extends Manager {
     }
 
     public function getJobApplications(Sector $sector,Profession $profession,Location $location,Contract $contract,Experience $experience,Level $level){
-        
-        // return $job_applications;
+        $sql = "SELECT * FROM job_application WHERE desccription LIKE %".$q."% ";
+        if(!is_null ($sector) && !is_null($profession) && !is_null($sector) && !is_null($location) && !is_null($contract) && !is_null($experience) && !is_null($level)){
+            $request = $this->pdo->query($sql);
+            $request->execute();
+        } else{
+             $filters = array();
+            if(!is_null ($sector)){
+                 $sql .= "AND id_sector = :id_sector";
+                array_merge($filters,array('id_sector' => $sector->getId()));
+            }
+            if(!is_null ($profession)){
+                 $sql .= "AND id_profession = :id_profession";
+                array_merge($filters,array('id_profession' => $profession->getId()));
+            }
+            if(!is_null ($location)){
+                 $sql .= "AND location = :location";
+                array_merge($filters,array('location' => $location));
+            }
+            if(!is_null ($contract)){
+                 $sql .= "AND type = :type";
+                array_merge($filters,array('type' => $contract->getType()));
+            }
+            if(!is_null ($experience)){
+                 $sql .= "AND min_experience = :min_experience";
+                array_merge($filters,array('min_experience' => $experience));
+            }
+            if(!is_null ($level)){
+                 $sql .= "AND level = :level";
+                array_merge($filters,array('level' => $level));
+            }
+            $request = $this->pdo->prepare($sql);
+            $request->execute($filters);
+        }
+        $job_applications = array();
+        while($response = $request->fetch(PDO::FETCH_OBJ))
+            $job_applications[] = $response;
+        return $job_applications;
     }
 
     public function update(JobApplication $job_application){
