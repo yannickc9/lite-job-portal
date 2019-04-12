f37abef691c2299fa6d25d39e664f60b6c0019e8
Yannick Choni <yannickjulberts@gmail.com>
2019-04-12 16:24:05 +0200

    (fix signin/index.php)

--- signin/index.php
+++ signin/index.php
@@ -43,29 +43,32 @@
                     </a>
                 </div>
                 <div class="login-form">
-                    <form>
+                    <form id="signin-form">
                         <div class="form-group">
                             <label>Nom complet *</label>
-                            <input type="text" id="fullname" name="fullname" class="form-control" placeholder="Prénom Nom Post-nom">
+                            <input type="text" id="fullname" name="fullname" class="form-control" placeholder="Prénom Nom Post-nom" required />
                         </div>
                         <div class="form-group">
                             <label>Adresse electronique *</label>
-                            <input type="email" id="email" name="email" class="form-control" placeholder="Adresse électronique">
+                            <input type="email" id="email" name="email" class="form-control" placeholder="Adresse électronique" required />
                         </div>
                         <div class="form-group">
                             <label>Mot de passe *</label>
-                            <input type="password" id="password" name="password" class="form-control" placeholder="Mot de passe">
+                            <input type="password" id="password" name="password" class="form-control" placeholder="Mot de passe" required />
                         </div>
                         <div class="form-group">
                             <label>Confirmer le mot de passe *</label>
-                            <input type="password" id="confirm_password" name="confirm_password" class="form-control" placeholder="Mot de passe">
+                            <input type="password" id="confirm_password" name="confirm_password" class="form-control" placeholder="Mot de passe" required />
+                        </div>
+                        <div id="passwords-warning" class="register-link m-t-15 d-none">
+                            <i class="fa fa-warning mr-2"></i><span>Les mots de passe ne correspondent pas!</span>
                         </div>
                         <div class="checkbox">
                             <label>
-                                <input type="checkbox"> J'accèpte les conditions d'utilisation et règles de confidentialités
+                                <input id="cbx_terms" type="checkbox" required /> J'accèpte les conditions d'utilisation et règles de confidentialités
                             </label>
                         </div>
-                        <button type="submit" id="btn_submit" class="btn btn-primary btn-flat m-b-30 m-t-30">S'inscrire</button>
+                        <input type="submit" id="btn_submit" class="btn btn-primary btn-flat m-b-30 m-t-30" value="S'inscrire" />
                         <div class="register-link m-t-15 text-center">
                             <p>Vous avez déjà un compte ? <a href="../page-login.html"> Connectez-vous ici</a></p>
                         </div>
@@ -76,12 +79,42 @@
     </div>
 
 
-    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <script src="../vendors/jquery/dist/jquery.js"></script>
+    <script>
+        fullname = $('#fullname'), email = $('#email'), password = $('#password'), confirm_password = $('#confirm_password'), btn_submit = $("#btn_submit");
+        // $("cbx_terms").change(function (e) {
+        //     alert('chane');
+        //     e.preventDefault();
+        //     btn_submit.removeClass('disabled');
+        // });
+        btn_submit.click(function(e) {
+            e.preventDefault();
+            if (password.val() != confirm_password.val())
+                $('#passwords-warning').removeClass('d-none');
+            else {
+                data = "fullname=" + fullname.val() + "&email=" + email.val() + "&password=" + password.val();
+                // alert(data);
+            // 
+            send(data);
+            // ).fail(
+            // function() {
+            // alert(data);
+            //             }
+            //         )
+            }
+        });
+
+        function send(data) {
+            $.post("test.php", data).done(
+                function(response) {
+                    alert(response);
+                }
+            )
+        }
+    </script>
     <script src="../vendors/popper.js/dist/umd/popper.min.js"></script>
     <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="../assets/js/main.js"></script>
-    <script src="../js/account/signin.script.js"></script>
-
 
 </body>
 
