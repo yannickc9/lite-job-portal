7fc5991973b4c72808730c4b7a5aaff7884a02b8
Yannick Choni <yannickjulberts@gmail.com>
2019-04-12 14:07:49 +0200

    (new file views/account-cv-experience) experience form

---
+++ views/account-cv-experience.php
@@ -0,0 +1,36 @@
+<div class="breadcrumbs">
+    <div class="col-sm-4">
+        <div class="page-header float-left">
+            <div class="page-title">
+                <h1>Ajouter une expérience</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content mt-3">
+    <div class="col-sm-6 p-2">
+        <form class="login-form">
+            <div class="form-group">
+                <label for="society">Société *</label>
+                <input type="text" id="society" name="society" class="form-control" placeholder="Nom de la socéié">
+            </div>
+            <div class="form-group">
+                <label for="type">Profession</label>
+                <input type="text" id="type" name="type" class="form-control" placeholder="Profession exercée ou poste occupé">
+            </div>
+            <div class="form-group">
+                <label for="bio">Déscription *</label>
+                <textarea type="text" id="bio" name="bio" class="form-control" placeholder="Décrivez votre métier"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="email">Début *</label>
+                <input type="email" id="email" name="email" class="form-control" placeholder="Date de début">
+            </div>
+            <div class="form-group">
+                <label for="telephone">Fin</label>
+                <input type="telephone" id="telephone" name="telephone" class="form-control" placeholder="Date de fin">
+            </div>
+            <button type="submit" class="btn btn-success btn-flat m-b-30 m-t-30">Ajouter</button>
+        </form>
+    </div>
+</div>
