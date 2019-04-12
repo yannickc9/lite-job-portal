0c60e5141c31180609f406a23b4b75a8c19263e8
Yannick Choni <yannickjulberts@gmail.com>
2019-04-12 15:47:24 +0200

    (new file views/account-cv-hobby.php)

---
+++ views/account-cv-hobby.php
@@ -0,0 +1,20 @@
+<div class="breadcrumbs">
+    <div class="col-sm-4">
+        <div class="page-header float-left">
+            <div class="page-title">
+                <h1>Ajouter un centre d'intérêt</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content mt-3">
+    <div class="col-sm-6 p-2">
+        <form class="login-form">
+            <div class="form-group">
+                <label for="hobby">Centre d'intérêt</label>
+                <input type="text" id="hobby" name="hobby" class="form-control" placeholder="Ajouter une passion, ce que vous aimez...">
+            </div>
+            <button type="submit" class="btn btn-success btn-flat m-b-30 m-t-30">Ajouter</button>
+        </form>
+    </div>
+</div>
