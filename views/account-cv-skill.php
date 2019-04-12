40cd04f53eb25f9cea29be87ef4f84b12639e2d9
Yannick Choni <yannickjulberts@gmail.com>
2019-04-12 15:45:35 +0200

    (new file views/account-cv-skill.php) new skill form

---
+++ views/account-cv-skill.php
@@ -0,0 +1,20 @@
+<div class="breadcrumbs">
+    <div class="col-sm-4">
+        <div class="page-header float-left">
+            <div class="page-title">
+                <h1>Ajouter une compétence</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content mt-3">
+    <div class="col-sm-6 p-2">
+        <form class="login-form">
+            <div class="form-group">
+                <label for="skill">Compétence</label>
+                <input type="text" id="skill" name="skill" class="form-control" placeholder="Décrivez votre compétence">
+            </div>
+            <button type="submit" class="btn btn-success btn-flat m-b-30 m-t-30">Ajouter</button>
+        </form>
+    </div>
+</div>
